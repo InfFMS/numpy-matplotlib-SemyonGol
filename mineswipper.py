4a7d7daff3e9419497ca9b6b9ff196de539ca69f
Semyon <s.golokolos@school.nsu.ru>
2025-01-25 18:06:42 +0700

    Доделал 3

--- mineswipper.py
+++ mineswipper.py
@@ -12,27 +12,132 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
 
-mine  = np.zeros((11, 11))
-m = 2
-c = 0
-dicC = {}
-dicA = {}
-dicB = {}
+from matplotlib.pyplot import legend
 
+#Создадим поле и введём необходимые переменные
+mine = np.zeros((11, 11))
+m, c = 2, 0
+dicC, dicX, dicY = {}, {}, {}
+
+#Разместим 15 мин в рандомные координаты поля
 while m < 17:
-    dicA[m-2] = randint(0, 10)
-    dicB[m-2] = randint(0, 10)
-    if c > 0:
+    dicX[m-2] = randint(0, 10)
+    dicY[m-2] = randint(0, 10)
+    if c > 0:       #Необходимо для создания второй пары координат(см. ниже)
         for i in range(0, m-2):
-            for x in range(i+1, m-1):
-                if dicA[i] == dicA[x] and dicB[i] == dicB[x]:
-                    dicA[x] = randint(0, 10)
-                    dicB[x] = randint(0, 10)
-    dicC[m-2] = plt.Circle((dicA[m-2], dicB[m-2]), 0.45, color='r', fill=True)
+            for o in range(i+1, m-1):
+                if dicX[i] == dicX[o] and dicY[i] == dicY[o]:   #Если пары координат мин совпадают, то они "сольются"
+                    dicX[o] = randint(0, 10)              #в одну мину. Для этого проверим на наличие таких пар
+                    dicY[o] = randint(0, 10)              #и изменим их на другие
+    dicC[m-2] = plt.Circle((dicX[m-2], dicY[m-2]), 0.45, color='r', fill=True)
     ax = plt.gca()
     ax.add_patch(dicC[m-2])
     m += 1
     c += 1
 
+#Создадим функции, которые будут увеличивать значение клетки
+#при различных положениях мины: по центру, слева, сверху, справа,
+#снизу, нижний левый угол, левый верхний, правый верхний, правый нижний
+def centre(M, Y, X):
+    M[Y-1, X-1] += 1
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    M[Y+1, X+1] += 1
+    M[Y, X+1] += 1
+    M[Y-1, X+1] += 1
+    M[Y-1, X] += 1
+    return M
+def left(M, Y, X):
+    M[Y-1, X] += 1
+    M[Y-1, X+1] += 1
+    M[Y, X+1] += 1
+    M[Y+1, X+1] += 1
+    M[Y+1, X] += 1
+    return M
+def top(M, Y, X):
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    M[Y+1, X+1] += 1
+    M[Y, X+1] += 1
+    return M
+def right(M, Y, X):
+    M[Y-1, X] += 1
+    M[Y-1, X-1] += 1
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    return M
+def bottom(M, Y, X):
+    M[Y, X-1] += 1
+    M[Y-1, X-1] += 1
+    M[Y-1, X] += 1
+    M[Y-1, X+1] += 1
+    M[Y, X+1] += 1
+    return M
+def bottomleft(M):
+    M[9, 0] += 1
+    M[9, 1] += 1
+    M[10, 1] += 1
+    return M
+def topleft(M):
+    M[1, 0] += 1
+    M[1, 1] += 1
+    M[0, 1] += 1
+    return M
+def topright(M):
+    M[0, 9] += 1
+    M[1, 9] += 1
+    M[1, 10] += 1
+    return M
+def bottomright(M):
+    M[10, 9] += 1
+    M[9, 9] += 1
+    M[9, 10] += 1
+    return M
+
+#Условия для активации верхних функций
+for i in range(0, 15):
+    if dicX[i] == 0:
+        if dicY[i] == 0:
+            print(topleft(mine))
+        if dicY[i] == 10:
+            print(bottomleft(mine))
+        else:
+            print(left(mine, dicY[i], dicX[i]))
+    elif dicY[i] == 0:
+        if dicX[i] == 10:
+            print(topright(mine))
+        elif dicX[i] == 0:
+            print(topleft(mine))
+        else:
+            print(top(mine, dicY[i], dicX[i]))
+    elif dicX[i] == 10:
+        if dicY[i] == 10:
+            print(bottomright(mine))
+        elif dicY[i] == 0:
+            print(topright(mine))
+        else:
+            print(right(mine, dicY[i], dicX[i]))
+    elif dicY[i] == 10:
+        if dicX[i] == 0:
+            print(bottomleft(mine))
+        if dicX[i] == 10:
+            print(bottomright(mine))
+        else:
+            print(bottom(mine, dicY[i], dicX[i]))
+    else:
+        print(centre(mine, dicY[i], dicX[i]))
+
+#Заменим значения клеток под минами на 0, так как
+#под ними клетка не может иметь числовое значение
+for i in range(0, 15):
+    mine[dicY[i], dicX[i]] = 0
+
+#Визуализируем всё поле
 plt.imshow(mine, cmap='Blues')
+plt.title('Сапёр')
+plt.xlabel('Чем холоднее синий цвет, тем больше мин около клетки')
+plt.ylabel('Красным отмечены мины')
 plt.show()
--- test.py
+++ test.py
@@ -1,8 +1,97 @@
-import matplotlib.pylab as plt
+from random import randint
 import numpy as np
-x = np.arange(10)
-y1 = np.random.randint(3, 20, len(x))
-y2 = np.random.randint(3, 20, len(x))
-w = 0.3
-x.bar(x - w/2, y1, width=w)
-x.bar(x + w/2, y2, width=w)
+
+M = np.zeros((11, 11))
+
+def centre(M, Y, X):
+    M[Y-1, X-1] += 1
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    M[Y+1, X+1] += 1
+    M[Y, X+1] += 1
+    M[Y-1, X+1] += 1
+    M[Y-1, X] += 1
+    return M
+def left(M, Y, X):
+    M[Y-1, X] += 1
+    M[Y-1, X+1] += 1
+    M[Y, X+1] += 1
+    M[Y+1, X+1] += 1
+    M[Y+1, X] += 1
+    return M
+def top(M, Y, X):
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    M[Y+1, X+1] += 1
+    M[Y, X+1] += 1
+    return M
+def right(M, Y, X):
+    M[Y-1, X] += 1
+    M[Y-1, X-1] += 1
+    M[Y, X-1] += 1
+    M[Y+1, X-1] += 1
+    M[Y+1, X] += 1
+    return M
+def bottom(M, Y, X):
+    M[Y, X-1] += 1
+    M[Y-1, X-1] += 1
+    M[Y-1, X] += 1
+    M[Y-1, X+1] += 1
+    M[Y, X+1] += 1
+    return M
+def bottomleft(M):
+    M[9, 0] += 1
+    M[9, 1] += 1
+    M[10, 1] += 1
+    return M
+def topleft(M):
+    M[1, 0] += 1
+    M[1, 1] += 1
+    M[0, 1] += 1
+    return M
+def topright(M):
+    M[0, 9] += 1
+    M[1, 9] += 1
+    M[1, 10] += 1
+    return M
+def bottomright(M):
+    M[10, 9] += 1
+    M[9, 9] += 1
+    M[9, 10] += 1
+    return M
+
+y = 5
+x = 0
+M[y, x] += 2
+if x == 0:
+    if y == 0:
+        print(topleft(M))
+    if y == 10:
+        print(bottomleft(M))
+    else:
+        print(left(M, y, x))
+elif y == 0:
+    if x == 10:
+        print(topright(M))
+    if x == 0:
+        print(topleft(M))
+    else:
+        print(top(M, y, x))
+elif x == 10:
+    if y == 10:
+        print(bottomright(M))
+    if y == 0:
+        print(topright(M))
+    else:
+        print(right(M, y, x))
+elif y == 10:
+    if x == 0:
+        print(bottomleft(M))
+    if x == 10:
+        print(bottomright(M))
+    else:
+        print(bottom(M, y, x))
+else:
+    print(centre(M, y, x))
